59a31c7be70b7ffe9fac7e3a55d9f3b0f61bf355
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-01-24 00:01:19 +0100

    Added lvb variables

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
 			        <?php include 'config.php'; ?>
                     <p align="center">
                         Die Waldjugend <?php echo $horst; ?> ist eine Ortsgruppe der 
-                        <a href="<?php echo $lvb_url; ?>"><?php echo $lvb; ?></a> - 
+                        <a href="//<?php echo $lvb_url; ?>"><?php echo $lvb; ?></a> - 
                         <a href="/impressum">Impressum</a>
                     </p>
 			    </div>
