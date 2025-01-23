84704674836a754e2fbf9570b4dafb1192bd7df9
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-01-23 23:18:34 +0100

    test

--- functions.php
+++ functions.php
@@ -11,22 +11,9 @@ function waldjugend_custom_login() {
 }
 add_action('login_head', 'waldjugend_custom_login');
 
-// Change login logo URL to homepage URL
-function waldjugend_custom_login_logo_url() {
-    return home_url();
+// Define THEME_TAGLINE from database
+function define_waldjugend_tagline() {
+    define('THEME_TAGLINE', get_option('blogdescription', 'default'));
 }
-add_filter('login_headerurl', 'waldjugend_custom_login_logo_url');
-
-// Fetch and set tagline dynamically, but only for config.php
-function waldjugend_set_config() {
-    // Only define the constant in config.php
-    if (basename(__FILE__) === 'config.php') {
-        // Get the tagline (blogdescription) from the database
-        $tagline = get_option('blogdescription', 'default');
-        
-        // Define the tagline as a constant, so it's globally available in config.php
-        define('THEME_TAGLINE', $tagline);
-    }
-}
-add_action('after_setup_theme', 'waldjugend_set_config');
+add_action('after_setup_theme', 'define_waldjugend_tagline');
 ?>
