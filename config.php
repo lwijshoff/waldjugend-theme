d49eab41fa3849cf7b572b2501192a2dfd192287
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-01-24 22:40:59 +0100

    Added ; to config.php

--- config.php
+++ config.php
@@ -16,7 +16,7 @@
  * @author Leonard Wijshoff
  */
 
-$horst = THEME_TAGLINE; // e.g. "Horst Kleve"
-$lvb = "Landesverband NRW e.V."; // e.g. "Deutschen Waldjugend NRW e.V."
-$lvb_url = "www.waldjugend-nrw.de"; // e.g. "www.waldjugend-nrw.de"
+$horst = THEME_TAGLINE; // e.g. "Horst Kleve";
+$lvb = "Landesverband NRW e.V."; // e.g. "Deutschen Waldjugend NRW e.V.";
+$lvb_url = "www.waldjugend-nrw.de"; // e.g. "www.waldjugend-nrw.de";
 ?>
