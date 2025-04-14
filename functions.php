37b27908b7f7f7e5416584eaaa1b24136765b838
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-04-14 15:08:10 +0200

    reworked functions.php

--- functions.php
+++ functions.php
@@ -1,51 +1,54 @@
 <?php
-// Enqueue parent theme styles
-function enqueue_parent_styles() {
+// Load Parent Theme Styles
+function waldjugend_enqueue_parent_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
-add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+add_action('wp_enqueue_scripts', 'waldjugend_enqueue_parent_styles');
 
-// Patterns delivered with the theme
-add_action('init', function() {
+// Load Block Patterns
+function waldjugend_register_block_patterns() {
     $pattern_dir = get_template_directory() . '/patterns';
 
     foreach (glob($pattern_dir . '/*.php') as $file) {
         register_block_pattern_from_file($file);
     }
-});
+}
+add_action('init', 'waldjugend_register_block_patterns');
 
 register_block_pattern_category(
     'waldjugend-buttons',
     ['label' => __('Waldjugend Buttons', 'waldjugend-theme')]
 );
 
-// Custom login for theme
-function waldjugend_custom_login() {
+// Custom Login Styling
+function waldjugend_custom_login_styles() {
     echo '<link rel="stylesheet" type="text/css" href="' . get_stylesheet_directory_uri() . '/css/waldjugendlogin.css" />';
 }
-add_action('login_head', 'waldjugend_custom_login');
+add_action('login_head', 'waldjugend_custom_login_styles');
 
 function waldjugend_login_logo_url() {
-    return home_url(); // Redirect to your site’s homepage
+    return home_url();
 }
 add_filter('login_headerurl', 'waldjugend_login_logo_url');
 
-// Define THEME_TAGLINE from database
-function define_waldjugend_tagline() {
-    define('THEME_TAGLINE', get_option('blogdescription', 'default'));
+// Define Theme Constants
+function waldjugend_define_constants() {
+    if (!defined('THEME_TAGLINE')) {
+        define('THEME_TAGLINE', get_option('blogdescription', 'default'));
+    }
 }
-add_action('after_setup_theme', 'define_waldjugend_tagline');
+add_action('after_setup_theme', 'waldjugend_define_constants');
 
-// Check for updates
-require 'inc/plugin-update-checker/plugin-update-checker.php';
+// Plugin/Theme Update Checker
+require_once get_template_directory() . '/inc/plugin-update-checker/plugin-update-checker.php';
+
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-$UpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/lwijshoff/waldjugend-theme/',
-	__FILE__,
-	'waldjugend-theme'
+$waldjugend_update_checker = PucFactory::buildUpdateChecker(
+    'https://github.com/lwijshoff/waldjugend-theme/',
+    __FILE__,
+    'waldjugend-theme'
 );
 
-//Set the branch that contains the stable release.
-$UpdateChecker->setBranch('dev');
+$waldjugend_update_checker->setBranch('dev');
 ?>
